ff658e533f91c3007a13d622514a2b203e9d0ff4
Anshul Goel <anshulgoel27@gmail.com>
2025-05-11 00:22:54 +0530

    Updated api key auth

--- gin/detector.go
+++ gin/detector.go
@@ -8,6 +8,8 @@ import (
 
 	auth "github.com/anshulgoel27/krakend-apikey-auth"
 	"github.com/gin-gonic/gin"
+	krakendjose "github.com/krakendio/krakend-jose/v2"
+	ginjose "github.com/krakendio/krakend-jose/v2/gin"
 	"github.com/luraproject/lura/v2/config"
 	"github.com/luraproject/lura/v2/logging"
 	"github.com/luraproject/lura/v2/proxy"
@@ -32,22 +34,22 @@ func NewApiKeyAuthenticator(ctx context.Context, cfg config.ServiceConfig, l log
 	return authManager, nil
 }
 
-func NewHandlerFactory(apiKeyLookupManager *auth.AuthKeyLookupManager, hf krakendgin.HandlerFactory, l logging.Logger) krakendgin.HandlerFactory {
+func NewHandlerFactory(apiKeyLookupManager *auth.AuthKeyLookupManager, hf krakendgin.HandlerFactory, l logging.Logger, rejecterF krakendjose.RejecterFactory) krakendgin.HandlerFactory {
 	return func(cfg *config.EndpointConfig, p proxy.Proxy) gin.HandlerFunc {
-		next := hf(cfg, p)
 		logPrefix := "[ENDPOINT: " + cfg.Endpoint + "][apikey-auth]"
-
 		detectorCfg, err := auth.ParseEndpointConfig(apiKeyLookupManager, cfg.ExtraConfig)
 		if err == auth.ErrNoConfig {
-			return next
+			hf := ginjose.HandlerFactory(hf, l, rejecterF)
+			return hf(cfg, p)
 		}
 		if err != nil {
 			l.Warning(logPrefix, err.Error())
-			return next
+			hf := ginjose.HandlerFactory(hf, l, rejecterF)
+			return hf(cfg, p)
 		}
 
 		d := auth.NewApiKeyAuthenticator(detectorCfg)
-		return handler(d, apiKeyLookupManager, next, l)
+		return handler(d, apiKeyLookupManager, hf(cfg, p), l)
 	}
 }
 
